321da8f457233616fa03e7156da358d31fe634c1
kevinwilde <kjwilde7@gmail.com>
2018-02-18 10:58:25 -0800

    Connect client and server

--- voting-client/src/index.jsx
+++ voting-client/src/index.jsx
@@ -3,21 +3,18 @@ import ReactDOM from 'react-dom';
 import {HashRouter, Route, Switch} from 'react-router-dom';
 import {createStore} from 'redux';
 import {Provider} from 'react-redux';
+import io from 'socket.io-client';
 import reducer from './reducer';
 import App from './components/App';
 import Results from './components/Results';
 import Voting from './components/Voting';
 
 const store = createStore(reducer);
-store.dispatch({
-  type: 'SET_STATE',
-  state: {
-    vote: {
-      pair: ['Sunshine', '28 Days Later'],
-      tally: {Sunshine: 2}
-    }
-  }
-});
+
+const socket = io(`${location.protocol}//${location.hostname}:9797`);
+socket.on('state', state =>
+  store.dispatch({type: 'SET_STATE', state})
+);
 
 ReactDOM.render(
   <Provider store={store}>
